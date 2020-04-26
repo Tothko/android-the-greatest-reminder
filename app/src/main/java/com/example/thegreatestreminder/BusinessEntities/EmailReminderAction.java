9bbc784faa45c49ee55d8cdacba2cd8e02fe8aa8
Marek Stancik <43235061+MarekStancik@users.noreply.github.com>
2020-04-26 15:52:08 +0200

    Few updates on entities

--- app/src/main/java/com/example/thegreatestreminder/BusinessEntities/EmailReminderAction.java
+++ app/src/main/java/com/example/thegreatestreminder/BusinessEntities/EmailReminderAction.java
@@ -2,6 +2,15 @@ package com.example.thegreatestreminder.BusinessEntities;
 
 public class EmailReminderAction extends ReminderAction {
 
+    String recipients[];
+    String message;
+
+    public EmailReminderAction(String recipients[],String message){
+        this.recipients = recipients;
+        this.message = message;
+    }
+
+
     @Override
     public Type getType() {
         return Type.EMAIL;
--- app/src/main/java/com/example/thegreatestreminder/BusinessEntities/Reminder.java
+++ app/src/main/java/com/example/thegreatestreminder/BusinessEntities/Reminder.java
@@ -10,6 +10,11 @@ public class Reminder {
     private Date triggerDate;
     private ReminderAction action;
 
+    public Reminder(String name,String detail,Date triggerDate){
+        this.name = name;
+        this.detail = detail;
+        this.triggerDate = triggerDate;
+    }
 
     public String getName() {
         return name;
